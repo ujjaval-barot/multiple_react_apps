8041a0512ad59c15f7291a717b2883b724b1e13c
ujjaval-barot <ujjavalbarot2@gmail.com>
2020-05-27 15:30:13 +0530

    combined multiple apps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Routes from "./routes/routes";
+import { Container } from "@material-ui/core";
 
 export default class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -8,17 +8,23 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import combineReducers from "./reducers";
 import promise from "redux-promise-middleware";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./sagas";
 
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-daterangepicker/daterangepicker.css";
+
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middleware = [thunk, promise];
+const middleware = [thunk, promise, sagaMiddleware];
 
 const store = createStore(
   combineReducers(),
   composeEnhancers(applyMiddleware(...middleware))
 );
-
+sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <App />
